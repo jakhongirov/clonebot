6e0ea427c7752dab29f0753b0e3bff28b60e07f1
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-11-15 20:00:06 +0800

    add

---
+++ model.js
@@ -0,0 +1,189 @@
+const {
+   fetch
+} = require('./src/lib/postgres')
+
+const foundUser = (chat_id) => {
+   const QUERY = `
+      SELECT 
+         *
+      FROM
+         users
+      WHERE
+         chat_id = $1;
+   `;
+
+   return fetch(QUERY, chat_id)
+}
+const foundCards = (chat_id) => {
+   const QUERY = `
+      SELECT 
+         count(id)
+      FROM
+         cards
+      WHERE
+         user_id = $1;
+   `;
+
+   return fetch(QUERY, chat_id)
+}
+const editStep = (chatId, step) => {
+   const QUERY = `
+      UPDATE
+         users
+      SET
+         step = $2
+      WHERE
+         chat_id = $1
+      RETURNING *;
+   `;
+
+   return fetch(QUERY, chatId, step)
+}
+const createUser = (
+   chatId,
+   step,
+   source
+) => {
+   const QUERY = `
+      INSERT INTO
+         users (
+            chat_id,
+            step
+         ) VALUES (
+            $1, 
+            $2
+         ) RETURNING *;
+   `;
+
+   return fetch(QUERY, chatId, step)
+}
+const editLang = (chatId, lang) => {
+   const QUERY = `
+      UPDATE
+         users
+      SET
+         lang = $2
+      WHERE
+         chat_id = $1
+      RETURNING *;
+   `;
+
+   return fetch(QUERY, chatId, lang)
+}
+const editName = (chatId, text) => {
+   const QUERY = `
+      UPDATE
+         users
+      SET
+         name = $2
+      WHERE
+         chat_id = $1
+      RETURNING *;
+   `;
+
+   return fetch(QUERY, chatId, text)
+}
+const addCardNumber = (chatId, text) => {
+   const QUERY = `
+      INSERT INTO
+         cards (
+            card_number,
+            user_id
+         ) VALUES (
+            $1, 
+            $2 
+         ) RETURNING *;
+   `;
+
+   return fetch(QUERY, chatId, text)
+}
+const addCardExpiry = (chatId, text) => {
+   const QUERY = `
+      UPDATE
+         cards
+      SET
+         expiry = $2
+      WHERE
+         user_id = $1
+      RETURNING *;
+   `;
+
+   return fetch(QUERY, chatId, text)
+}
+const addCardTransId = (chatId, transaction_id) => {
+   const QUERY = `
+      UPDATE
+         cards
+      SET
+         transaction_id = $2
+      WHERE
+         user_id = $1
+      RETURNING *;
+   `;
+
+   return fetch(QUERY, chatId, transaction_id)
+}
+const foundCard = (chat_id) => {
+   const QUERY = `
+      SELECT 
+         *
+      FROM
+         cards
+      WHERE
+         user_id = $1;
+   `;
+
+   return fetch(QUERY, chat_id)
+}
+const addCardData = (
+   chatId,
+   card_id,
+   pen,
+   expiry,
+   card_holder,
+   balance,
+   phone,
+   card_token
+) => {
+   const QUERY = `
+      UPDATE
+         cards
+      SET
+         card_id = $2,
+         card_number_hash = $3,
+         expiry = $4,
+         card_holder = $5,
+         balance = $6,
+         phone_number = $7,
+         card_token = $8
+      WHERE
+         user_id = $1
+      RETURNING *;
+   `;
+
+   return fetch(
+      QUERY,
+      chatId,
+      card_id,
+      pen,
+      expiry,
+      card_holder,
+      balance,
+      phone,
+      card_token
+   )
+}
+
+module.exports = {
+   foundUser,
+   foundCards,
+   editStep,
+   createUser,
+   editLang,
+   editName,
+   addCardNumber,
+   addCardExpiry,
+   addCardTransId,
+   foundCard,
+   addCardData
+}
---
+++ src/model/model.sql
@@ -0,0 +1,54 @@
+CREATE TABLE admins (
+   admin_id bigserial PRiMARY KEY,
+   admin_email text not null,
+   admin_password text not null,
+   admin_role text DEFAULT 'admin' admin_create_at timestamptz DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE users (
+   id bigserial,
+   chat_id bigint PRIMARY KEY,
+   phone_number text,
+   name text,
+   step text DEFAULT 'start',
+   subscribe boolean DEFAULT false,
+   expired text,
+   source text,
+   lang text,
+   trial int DEFAULT 1,
+   duration boolean DEFAULT false,
+   create_at timestamptz DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE atmos_token (
+   id bigserial PRIMARY KEY,
+   token text,
+   expires int
+);
+
+CREATE TABLE cards (
+   id bigserial PRIMARY KEY,
+   card_number_hash text,
+   card_number text,
+   card_id int,
+   expiry text,
+   otp int,
+   card_token text,
+   phone_number text,
+   card_holder text,
+   balance int,
+   transaction_id int,
+   user_id bigint REFERENCES users(chat_id) ON DELETE CASCADE,
+   create_at timestamptz DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE transaction (
+   id bigserial PRIMARY KEY,
+   user_id bigint REFERENCES users(chat_id) ON DELETE CASCADE,
+   success_trans_id text,
+   method text,
+   amount bigint,
+   transaction_id int,
+   ofd_url text,
+   create_at timestamptz DEFAULT CURRENT_TIMESTAMP
+);
